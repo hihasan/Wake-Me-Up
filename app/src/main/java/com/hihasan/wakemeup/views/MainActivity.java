df5799d07c980d55332b483a46df4cc612bacb59
hihasan <hasan@hihasan.com>
2019-05-26 22:44:33 +0600

    files

--- app/src/main/java/com/hihasan/wakemeup/views/MainActivity.java
+++ app/src/main/java/com/hihasan/wakemeup/views/MainActivity.java
@@ -1,11 +1,13 @@
 package com.hihasan.wakemeup.views;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -15,12 +17,16 @@ import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import com.github.clans.fab.FloatingActionMenu;
+import com.hihasan.prisom.toaster.Toaster;
 import com.hihasan.wakemeup.R;
 import com.hihasan.wakemeup.adapter.ContentAdapter;
 import com.hihasan.wakemeup.utils.Utils;
 
 public class MainActivity extends AppCompatActivity
 {
+    private FloatingActionMenu fab_menu;
+    private com.github.clans.fab.FloatingActionButton add,faq,about_us;
 
     private Context context=this;
     private RecyclerView recycler;
@@ -33,16 +39,12 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-                Intent i=new Intent(getApplicationContext(),SettingsActivity.class);
-                startActivity(i);
-            }
-        });
+        fab_menu=(FloatingActionMenu) findViewById (R.id.fabmenu);
+        add=(com.github.clans.fab.FloatingActionButton) findViewById (R.id.add);
+        faq=(com.github.clans.fab.FloatingActionButton) findViewById (R.id.faq);
+        about_us=(com.github.clans.fab.FloatingActionButton) findViewById (R.id.about_us);
+
+        fabAction();
 
         Utils.model();
 
@@ -50,5 +52,41 @@ public class MainActivity extends AppCompatActivity
         adapter=new ContentAdapter(Utils.model);
         recycler.setLayoutManager(new LinearLayoutManager(this));
         recycler.setAdapter(adapter);
+    }
+
+    private void fabAction()
+    {
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final Dialog dialog=new Dialog(context);
+                dialog.setContentView(R.layout.activity_info);
+
+                AppCompatButton cancel=(AppCompatButton) dialog.findViewById(R.id.cancel);
+                cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+            }
+        });
+
+        faq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toaster.makeText(getApplicationContext(),"Action Needed", Toaster.INFO,true);
+            }
+        });
+
+        about_us.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toaster.makeText(getApplicationContext(), "Action Needed", Toaster.INFO, true);
+            }
+        });
     }
 }
